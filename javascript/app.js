7a520917a81875cf420851c2233521e06bae4078
John Bendfeldt <jfbendfeldt@gmail.com>
2017-05-25 19:11:53 -0500

    fixed time format

--- javascript/app.js
+++ javascript/app.js
@@ -38,6 +38,32 @@ $('#add-train-btn').on('click', function(event) {
   $('#first-train-input').val('');
   $('#frequency-input').val('');
 });
+
+//limits time input to valid time
+$('#first-train-input').on('keyup change', function() {
+  console.log('the time has changed');
+  var currentVal = $(this).val();
+  var cleanVal = currentVal.replace(':', '');
+  console.log(currentVal.length)
+  if(cleanVal.length === 4) {
+   var firstTwo = cleanVal.substring(0, 2);
+   var lastTwo = cleanVal.substring(2, 4);
+   console.log(firstTwo, lastTwo)
+   if (parseInt(firstTwo) < 0 || parseInt(firstTwo) > 24 || parseInt(lastTwo) < 0 || parseInt(lastTwo) > 59) {
+      $(this).val('');
+   }else {
+      $(this).val(firstTwo + ':' + lastTwo)
+   }
+   if (firstTwo == 24) {
+      $(this).val(firstTwo + ':' + '00')
+   }
+  }
+  //if cleanVal longer than 4 it deletes the last value limiting it to 4
+  if(cleanVal.length > 4) {
+    $(this).val(currentVal.substring(0, currentVal.length - 1));
+  }
+})
+
 //creates child in database and creates row with input
 database.ref().on('child_added', function(childSnapshot, prevChildKey) {
   //establishes childSnapshot variables
@@ -47,13 +73,13 @@ database.ref().on('child_added', function(childSnapshot, prevChildKey) {
   var cSfrequency = childSnapshot.val().dBfrequency;
 
   //establishes times for trains using moment
-  var firstTimeConverted = moment(cSfirstTrainTime, 'hh:mm').subtract(1, 'years');
+  var firstTimeConverted = moment(cSfirstTrainTime, 'HH:mm').subtract(1, 'years');
   var currentTime = moment();
   var diffTime = moment().diff(moment(firstTimeConverted), 'minutes');
   var tRemainder = diffTime % cSfrequency;
   var minutesAway = cSfrequency - tRemainder;
   var calcNextArrival = moment().add(minutesAway, 'minutes');
-  var nextArrival = moment(calcNextArrival).format('hh:mm');
+  var nextArrival = moment(calcNextArrival).format('HH:mm');
 
   //append train data to the table
   $('table > tbody').append('<tr><td>' + cStrainName + '</td><td>' + cSdestination + '</td><td>Every ' +
